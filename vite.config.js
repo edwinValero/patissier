76b661452608ca02eb01d32bef48cbaabf002b3f
edwin valero <edwin.valero@teaminternational.com>
2025-07-18 13:51:33 -0400

    remove gh-pages plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { ghPages } from 'vite-plugin-gh-pages';
+//import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/patissier/',
-  plugins: [react(), tailwindcss(), ghPages()],
+  plugins: [react(), tailwindcss()],
 });
